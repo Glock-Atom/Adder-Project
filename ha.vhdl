2c1a61df245374e2de72b34e3d39f39f47639697
Hickok Kinman <58397806+Glock-Atom@users.noreply.github.com>
2021-12-30 18:48:29 +0100

    Add files via upload

---
+++ ha.vhdl
@@ -0,0 +1,30 @@
+-----------------------------------------------------------------
+-- Project: Halbaddierer
+-----------------------------------------------------------------
+-- File:   ha.vhdl 
+-- Author: Hamza Tekin
+-- Date:   October 13, 2021
+-----------------------------------------------------------------
+-- half-adder
+-----------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity ha is
+port(
+  a : in std_logic;
+  b : in std_logic;
+  s : out std_logic; --sum
+  co : out std_logic --carry out
+  );    
+end ha;
+
+architecture dataflow of ha is
+
+-- declaration part
+
+begin -- architecture body
+  s <= a xor b;
+  co <= a and b;
+end dataflow;
